dd5001f82a6c28106e11b8fe68d76113a9daf597
Aminessid <essidmohamedamin@gmail.com>
2020-06-25 16:13:27 +0000

    create ejs files and load the css, dom manipulation and images to pages

--- app.js
+++ app.js
@@ -3,47 +3,61 @@ const express = require('express');
 // express app
 const app = express();
 
+// registre view engine
+app.set('view engine', 'ejs');
+
 // listen to requests
 app.listen(3000);
 
+// middleware and static files
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
     // res.send('...');
-    res.sendFile('./views/home/index.html', { root: __dirname})
+    // res.sendFile('./views/home/index.html', { root: __dirname})
+    res.render('./home/index', {title: 'home'});
 });
 
 app.get('/ConnectFour', (req, res) => {
     // res.send('...');
-    res.sendFile('./views/ConnectFour/index.html', { root: __dirname})
+    // res.sendFile('./views/ConnectFour/index.html', { root: __dirname});
+    res.render('./ConnectFour/index', {title: 'ConnectFour'});
 });
 
 app.get('/DeathRace', (req, res) => {
     // res.send('...');
-    res.sendFile('./views/DeathRace/index.html', { root: __dirname})
+    // res.sendFile('./views/DeathRace/index.html', { root: __dirname});
+    res.render('./DeathRace/index', {title: 'DeathRace'});
 });
 
 app.get('/MemoryGame', (req, res) => {
     // res.send('...');
-    res.sendFile('./views/MemoryGame/index.html', { root: __dirname})
+    // res.sendFile('./views/MemoryGame/index.html', { root: __dirname})
+    res.render('./MemoryGame/index', {title: 'MemoryGame'});
 });
 
 app.get('/SpaceInvader', (req, res) => {
     // res.send('...');
-    res.sendFile('./views/SpaceInvader/index.html', { root: __dirname})
+    // res.sendFile('./views/SpaceInvader/index.html', { root: __dirname});
+    res.render('./SpaceInvader/index', {title: 'SpaceInvader'});
 });
 
 app.get('/Tetris', (req, res) => {
     // res.send('...');
-    res.sendFile('./views/Tetris/index.html', { root: __dirname})
+    // res.sendFile('./views/Tetris/index.html', { root: __dirname});
+    res.render('./Tetris/index', {title: 'Tetris'});
 });
 
 app.get('/TheSnake', (req, res) => {
     // res.send('...');
-    res.sendFile('./views/TheSnake/index.html', { root: __dirname})
+    // res.sendFile('./views/TheSnake/index.html', { root: __dirname});
+    res.render('./TheSnake/index', {title: 'TheSnake'});
 });
 
 app.get('/WackMole', (req, res) => {
     // res.send('...');
-    res.sendFile('./views/WackMole/index.html', { root: __dirname})
+    // res.sendFile('./views/WackMole/index.html', { root: __dirname});
+    res.render('./WackMole/index', {title: 'WackMole'});
 });
 
 // app.get('/', (req, res) => {
@@ -53,5 +67,6 @@ app.get('/WackMole', (req, res) => {
 
 // 404 page
 app.use((req, res) => {
-    res.status(404).sendFile('views/404/index.html', { root: __dirname})
+    // res.status(404).sendFile('views/404/index.html', { root: __dirname})
+    res.status(404).render('./404/index', {title: '404'});
 })
--- public/MemoryGame.js
+++ public/MemoryGame.js
@@ -4,51 +4,51 @@
     const cardArray =[
         {
             name: "rocket1",
-            img: "images/img 1.png"
+            img: "./MemoryGame-images/img 1.png"
         },
         {
             name: "rocket2",
-            img: "images/img 1.png"
+            img: "./MemoryGame-images/img 1.png"
         },
         {
             name: "boat1",
-            img: "images/img 2.png"
+            img: "./MemoryGame-images/img 2.png"
         },
         {
             name: "boat2",
-            img: "images/img 2.png"
+            img: "./MemoryGame-images/img 2.png"
         },
         {
             name: "ballon1",
-            img: "images/img 3.png"
+            img: "./MemoryGame-images/img 3.png"
         },
         {
             name: "ballon2",
-            img: "images/img 3.png"
+            img: "./MemoryGame-images/img 3.png"
         },
         {
             name: "bike1",
-            img: "images/img 4.png"
+            img: "./MemoryGame-images/img 4.png"
         },
         {
             name: "bike2",
-            img: "images/img 4.png"
+            img: "./MemoryGame-images/img 4.png"
         },
         {
             name: "helicopter1",
-            img: "images/img 5.png"
+            img: "./MemoryGame-images/img 5.png"
         },
         {
             name: "helicopter2",
-            img: "images/img 5.png"
+            img: "./MemoryGame-images/img 5.png"
         },
         {
             name: "plane1",
-            img: "images/img 6.png"
+            img: "./MemoryGame-images/img 6.png"
         },
         {
         name: "plane2",
-        img: "images/img 6.png"
+        img: "./MemoryGame-images/img 6.png"
     }];
 
     cardArray.sort(() => 0.5 - Math.random());
@@ -78,7 +78,7 @@
     function createBoard () {
         for (let i = 0; i<cardArray.length; i++){
             let card = document.createElement("img");
-            card.setAttribute("src", "images/cover.png");
+            card.setAttribute("src", "./MemoryGame-images/cover.png");
             card.setAttribute("data-id", i);
             card.addEventListener("click", flipCard);
             grid.appendChild(card);
@@ -92,13 +92,13 @@
         let optionOneId = chosenCardsId[0], optionTwoId = chosenCardsId[1];
         if (chosenCards[0].slice(0, (chosenCards[0].length - 1)) === chosenCards[1].slice(0, (chosenCards[1].length - 1)) && chosenCards[0].slice((chosenCards[0].length - 1), chosenCards[0].length) !== chosenCards[1].slice((chosenCards[1].length - 1), chosenCards[1].length) ){
             alert("nice you found a match");
-            cards[optionOneId].setAttribute("src", "images/empty.png");
-            cards[optionTwoId].setAttribute("src", "images/empty.png");
+            cards[optionOneId].setAttribute("src", "./MemoryGame-images/empty.png");
+            cards[optionTwoId].setAttribute("src", "./MemoryGame-images/empty.png");
             cardsWon.push(chosenCards);
         } else {
             alert("sorry try again")
-            cards[optionOneId].setAttribute("src", "images/cover.png");
-            cards[optionTwoId].setAttribute("src", "images/cover.png"); 
+            cards[optionOneId].setAttribute("src", "./MemoryGame-images/cover.png");
+            cards[optionTwoId].setAttribute("src", "./MemoryGame-images/cover.png"); 
         }
         chosenCards = [];
         chosenCardsId = [];
