68a7cbb7c51676e6e9a3aa2ce1e018491b48f509
Aminessid <essidmohamedamin@gmail.com>
2020-08-08 19:36:59 +0000

    fix bug 1

--- server.js
+++ server.js
@@ -14,60 +14,34 @@ app.listen(port);
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-    // res.send('...');
-    // res.sendFile('./views/home/index.html', { root: __dirname})
     res.render('./home/index', {title: 'home'});
 });
 
-// app.get('/ConnectFour', (req, res) => {
-//     // res.send('...');
-//     // res.sendFile('./views/ConnectFour/index.html', { root: __dirname});
-//     res.render('./ConnectFour/index', {title: 'ConnectFour'});
-// });
-
 app.get('/Minesweeper', (req, res) => {
-    // res.send('...');
-    // res.sendFile('./views/DeathRace/index.html', { root: __dirname});
     res.render('./Minesweeper/index', {title: 'Minesweeper'});
 });
 
 app.get('/MemoryGame', (req, res) => {
-    // res.send('...');
-    // res.sendFile('./views/MemoryGame/index.html', { root: __dirname})
     res.render('./MemoryGame/index', {title: 'MemoryGame'});
 });
 
 app.get('/SpaceInvader', (req, res) => {
-    // res.send('...');
-    // res.sendFile('./views/SpaceInvader/index.html', { root: __dirname});
     res.render('./SpaceInvader/index', {title: 'SpaceInvader'});
 });
 
 app.get('/Tetris', (req, res) => {
-    // res.send('...');
-    // res.sendFile('./views/Tetris/index.html', { root: __dirname});
     res.render('./Tetris/index', {title: 'Tetris'});
 });
 
 app.get('/TheSnake', (req, res) => {
-    // res.send('...');
-    // res.sendFile('./views/TheSnake/index.html', { root: __dirname});
     res.render('./TheSnake/index', {title: 'TheSnake'});
 });
 
 app.get('/WackMole', (req, res) => {
-    // res.send('...');
-    // res.sendFile('./views/WackMole/index.html', { root: __dirname});
     res.render('./WackMole/index', {title: 'WackMole'});
 });
 
-// app.get('/', (req, res) => {
-//     // res.send('...');
-//     res.sendFile('./views/home/index.html', { root: __dirname})
-// });
-
 // 404 page
 app.use((req, res) => {
-    // res.status(404).sendFile('views/404/index.html', { root: __dirname})
     res.status(404).render('./404/index', {title: '404'});
 })
